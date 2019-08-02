90a8c4c969dc2309105c198fb8114a5bd9f67f5d
Timur Ramazanov <charlie.vmk@gmail.com>
2019-08-02 18:06:18 +0300

    Generate address with given prefix

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate clap;
 extern crate stellar_vanity;
 
 use clap::{App, Arg};
+use stellar_vanity::vanity_key::AddressGenerator;
 
 fn main() {
     let matches = App::new("Stellar Vanity Address Generator")
@@ -10,18 +11,46 @@ fn main() {
         .about("A simple CLI for generating Stellar Vanity Addresses.")
         .arg(
             Arg::with_name("POSTFIX")
-                .required(true)
+                .long("postfix")
                 .takes_value(true)
-                .index(1)
-                .help("desired address postfix"),
+                .help("desired address suffix"),
+        )
+        .arg(
+            Arg::with_name("PREFIX")
+                .long("prefix")
+                .takes_value(true)
+                .help("desired address prefix"),
         )
         .get_matches();
-    let postfix = matches.value_of("POSTFIX").unwrap();
+
+    let postfix_option = matches.value_of("POSTFIX");
+    let prefix_option = matches.value_of("PREFIX");
+
+    if postfix_option.is_none() && prefix_option.is_none() {
+        eprintln!("\n Please, provide prefix or postfix");
+        ::std::process::exit(1);
+    }
+
+    let mut generator: AddressGenerator = Default::default();
 
     println!("\nSEARCHING INITIATED");
 
-    let (public_key, private_key) =
-        stellar_vanity::vanity_key::generate_vanity_key(&postfix.to_uppercase());
+    let (public_key, private_key) = generator
+        .find(|(pk, _)| {
+            let mut found = true;
+            let key_str = pk.as_str();
+
+            if let Some(postfix) = postfix_option {
+                found &= key_str.ends_with(postfix);
+            }
+
+            if let Some(prefix) = prefix_option {
+                found &= key_str[2..].starts_with(prefix);
+            }
+
+            found
+        })
+        .unwrap();
 
     println!(
         "\nSUCCESS!\nPublic Key: {:?}\nSecret Key: {:?}",
